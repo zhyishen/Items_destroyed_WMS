13636782efb5bf8f008b24d92be6e38183bf61d2
zhyishen <zhyishen@163.com>
2023-01-12 11:04:04 +0800

    test

--- src/main/java/com/safetychina/items_destroyed_wms/ItemsDestroyedWmsApplication.java
+++ src/main/java/com/safetychina/items_destroyed_wms/ItemsDestroyedWmsApplication.java
@@ -10,6 +10,7 @@ public class ItemsDestroyedWmsApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ItemsDestroyedWmsApplication.class, args);
+        test
     }
 
 }
