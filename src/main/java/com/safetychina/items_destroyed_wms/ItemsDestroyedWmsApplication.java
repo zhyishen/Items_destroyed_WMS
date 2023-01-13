3f27fd9060af8a37e358c67d345bf5e8e9190837
zhyishen <zhyishen@163.com>
2023-01-13 18:59:28 +0800

    connect to database

--- src/main/java/com/safetychina/items_destroyed_wms/ItemsDestroyedWmsApplication.java
+++ src/main/java/com/safetychina/items_destroyed_wms/ItemsDestroyedWmsApplication.java
@@ -2,9 +2,10 @@ package com.safetychina.items_destroyed_wms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ItemsDestroyedWmsApplication {
 
     public static void main(String[] args) {
