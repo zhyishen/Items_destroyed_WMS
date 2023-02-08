44a76c31fe73cebe9e8177ba96497a23cf0225ea
zhyishen <zhyishen@163.com>
2023-02-08 11:17:54 +0800

    fisrt page finish

--- vue_ui/App.vue
+++ vue_ui/App.vue
@@ -1,19 +1,89 @@
 <script setup>
 
+    import {ref} from "vue";
+    // 数据
+    let queryInput = ref("")
+    let tableData = ref([{
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },])
+    // 方法
+    const handleClick = () =>{
+        console.log('click')
+    }
+
 </script>
 
 <template>
-    <el-row class="mb-4">
-        <el-button>Default</el-button>
-        <el-button type="primary">Primary</el-button>
-        <el-button type="success">Success</el-button>
-        <el-button type="info">Info</el-button>
-        <el-button type="warning">Warning</el-button>
-        <el-button type="danger">Danger</el-button>
-    </el-row>
+    <div class="table-box">
+        <div class="title">
+            <h2>非涉密文件</h2>
+        </div>
+        <!-- query -->
+        <div class="query-box">
+            <el-input v-model="queryInput" placeholder="Please input" />
+            <el-button type="primary">增加</el-button>
+        </div>
+        <!-- table -->
+        <div class="table">
+            <el-table :data="tableData" style="width: 100%">
+                <el-table-column fixed prop="date" label="Date" width="150" />
+                <el-table-column prop="name" label="Name" width="120" />
+                <el-table-column prop="state" label="State" width="120" />
+                <el-table-column prop="city" label="City" width="120" />
+                <el-table-column prop="address" label="Address" width="600" />
+                <el-table-column prop="zip" label="Zip" width="120" />
+                <el-table-column fixed="right" label="Operations" width="120">
+                    <template #default>
+                        <el-button link type="primary" size="small" @click="handleClick">Detail</el-button>
+                        <el-button link type="primary" size="small">Edit</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+    </div>
+    
 </template>
 
 
-<style>
-
+<style scoped>
+.table-box{
+    width: 800px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>
