68fe005fd70e093e5ab4d2c873d079264c756a60
zhyishen <zhyishen@163.com>
2023-02-15 15:41:38 +0800

    separate view from main app

--- vue_ui/App.vue
+++ vue_ui/App.vue
@@ -1,13 +1,7 @@
 <script setup>
 
     import {ref} from "vue";
-    import NonConfiditialDocumentGrid from "@/components/NonConfiditialDocuments/NonConfidentialDocumentGrid.vue";
-    import NonConfiditialDocumentDialog from "@/components/NonConfiditialDocuments/NonConfidentialDocumentDialog.vue";
-    import NonConfiditialDocumentQueryBox from "@/components/NonConfiditialDocuments/NonConfidentialDocumentQueryBox.vue";
-
-    import {useNonConfidentialDocumentsStore} from '@/store/nonConfidentialDocumentStore'
-    
-    const nfs = useNonConfidentialDocumentsStore()
+    import NonConfidentialDocumentView from "@/views/NonConfidentialDocumentView.vue";
 
 
     
@@ -21,50 +15,11 @@
 </script>
 
 <template>
-    <div class="table-box">
-        <div class="title">
-            <h2>非涉密文件</h2>
-        </div>
-        <!-- query -->
-        <div class="query-box">
-
-          <NonConfiditialDocumentQueryBox></NonConfiditialDocumentQueryBox>
-            
-        </div>
-
-
-        <div class="table">
-
-          <NonConfiditialDocumentGrid></NonConfiditialDocumentGrid>
-
-        </div>
-
-        <div class="dialog">
-
-          <NonConfiditialDocumentDialog></NonConfiditialDocumentDialog>
-
-        </div>
-        
-
-        
-    </div>
+    <NonConfidentialDocumentView></NonConfidentialDocumentView>
     
 </template>
 
 
 <style scoped>
-.table-box{
-    width: 800px;
-    margin: 200px auto;
-}
-.title{
-    text-align: center;
-}
-.query-box{
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 20px;
-}
-
 
 </style>
--- vue_ui/src/views/NonConfidentialDocumentView.vue
+++ vue_ui/src/views/NonConfidentialDocumentView.vue
@@ -1,13 +1,59 @@
+<script setup>
+
+import {ref} from "vue";
+import NonConfiditialDocumentGrid from "@/components/NonConfiditialDocuments/NonConfidentialDocumentGrid.vue";
+import NonConfiditialDocumentDialog from "@/components/NonConfiditialDocuments/NonConfidentialDocumentDialog.vue";
+import NonConfiditialDocumentQueryBox from "@/components/NonConfiditialDocuments/NonConfidentialDocumentQueryBox.vue";
+
+// 方法
+
+</script>
+
 <template>
+  <div class="table-box">
+    <div class="title">
+      <h2>非涉密文件</h2>
+    </div>
+    <!-- query -->
+    <div class="query-box">
+
+      <NonConfiditialDocumentQueryBox></NonConfiditialDocumentQueryBox>
+
+    </div>
+
+
+    <div class="table">
+
+      <NonConfiditialDocumentGrid></NonConfiditialDocumentGrid>
+
+    </div>
+
+    <div class="dialog">
+
+      <NonConfiditialDocumentDialog></NonConfiditialDocumentDialog>
+
+    </div>
+
+
+
+  </div>
 
 </template>
 
-<script>
-export default {
-  name: "NonConfidentialDocumentView"
-}
-</script>
 
 <style scoped>
+.table-box{
+  width: 800px;
+  margin: 200px auto;
+}
+.title{
+  text-align: center;
+}
+.query-box{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
 
 </style>
