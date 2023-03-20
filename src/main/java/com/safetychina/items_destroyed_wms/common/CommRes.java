297c381026cb7c31722a58fc82e1061e9a38a76a
zhyishen <zhyishen@163.com>
2023-03-20 23:14:45 +0800

    add validation advice

--- src/main/java/com/safetychina/items_destroyed_wms/common/CommRes.java
+++ src/main/java/com/safetychina/items_destroyed_wms/common/CommRes.java
@@ -4,7 +4,35 @@ import lombok.Data;
 
 @Data
 public class CommRes {
-    private String code;
+    private int code;
     private String msg;
     private Object data;
+
+    // 手动设置返回vo
+    public CommRes(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    // 默认返回成功状态码，数据对象
+    public CommRes(Object data) {
+        this.code = ResultCode.SUCCESS.getCode();
+        this.msg = ResultCode.SUCCESS.getMsg();
+        this.data = data;
+    }
+
+    // 返回指定状态码，数据对象
+    public CommRes(StatusCode statusCode, Object data) {
+        this.code = statusCode.getCode();
+        this.msg = statusCode.getMsg();
+        this.data = data;
+    }
+
+    // 只返回状态码
+    public CommRes(StatusCode statusCode) {
+        this.code = statusCode.getCode();
+        this.msg = statusCode.getMsg();
+        this.data = null;
+    }
 }
--- src/main/java/com/safetychina/items_destroyed_wms/exception/ValidationExceptionAdvice.java
+++ src/main/java/com/safetychina/items_destroyed_wms/exception/ValidationExceptionAdvice.java
@@ -1,9 +1,44 @@
 package com.safetychina.items_destroyed_wms.exception;
 
+import com.safetychina.items_destroyed_wms.common.CommRes;
+import com.safetychina.items_destroyed_wms.common.ResultCode;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Set;
 
 @RestControllerAdvice
 public class ValidationExceptionAdvice {
 
+    //用于捕获@RequestParam/@PathVariable参数触发校验规则抛出的异常
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public CommRes handleConstraintViolationException(ConstraintViolationException e) {
+        StringBuilder sb = new StringBuilder();
+        Set<ConstraintViolation<?>> conSet = e.getConstraintViolations();
+        for (ConstraintViolation<?> con : conSet) {
+            String message = con.getMessage();
+            sb.append(message).append(";");
+        }
+        return new CommRes(ResultCode.VALIDATE_ERROR,sb.toString());
+    }
+
+    //用于捕获@RequestBody类型参数触发校验规则抛出的异常
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommRes handleValidException(MethodArgumentNotValidException e) {
+        StringBuilder sb = new StringBuilder();
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        if (!CollectionUtils.isEmpty(allErrors)) {
+            for (ObjectError error : allErrors) {
+                sb.append(error.getDefaultMessage()).append(";");
+            }
+        }
+        return new CommRes(ResultCode.VALIDATE_ERROR,sb.toString());
+    }
 
 }
